bb1febdee6fad943536c7e7ff675bd6fd3d3db0e
calix00 <dhk08300@gmail.com>
2025-06-29 03:09:50 +0900

    Row title modify

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -8,7 +8,7 @@ export default function MainPage() {
     <div>
       <Banner />
 
-      <Row title='오늘 대한민국의 TOP 20' id='TR' fetchUrl={requests.fetchTopRated} />
+      <Row title='전 세계 TOP 10' id='TR' fetchUrl={requests.fetchTopRated} />
       <Row title='넷플릭스 오리지널' id='NO'fetchUrl={requests.fetchNetflixOriginals}  />
       <Row title='지금 뜨는 콘텐츠' id='TN' fetchUrl={requests.fetchTrending} />
       <Row title='액션 & 어드벤쳐' id='AM' fetchUrl={requests.fetchActionMovies} />
